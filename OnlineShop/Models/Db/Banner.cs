599449ee8dd86350abac3a993b54b5b124a6dcd3
DejnielST <dejni@DESKTOP-CQ726VA>
2025-01-08 21:32:47 +0100

    Added new db.Banner to Models\Db

---
+++ OnlineShop/Models/Db/Banner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlineShop.Models.Db;
+
+public partial class Banner
+{
+    public string? Title { get; set; }
+
+    public string? SubTitle { get; set; }
+
+    public string? ImageName { get; set; }
+
+    public short? Priority { get; set; }
+
+    public string? Link { get; set; }
+
+    public string? Position { get; set; }
+}
